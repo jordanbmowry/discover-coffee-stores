7294ee5ebfdd2126c828cd2f617b628b0574611b
Jordan Mowry <jordan.mowry@gmail.com>
2022-01-24 13:49:19 -0700

    added clsx npm package to project to manage classNames and added more styles to Card component.

--- components/Card.js
+++ components/Card.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import styles from './Card.module.css';
+import clsx from 'clsx';
 
 export default function Card(props) {
+  console.log(styles);
   return (
     <Link href={props.href}>
-      <a>
-        <h2>{props.name}</h2>
-        <Image alt='coffee shop' src={props.imgUrl} width={260} height={160} />
+      <a className={styles.cardLink}>
+        <div className={clsx('glass', styles.container)}>
+          <div className={styles.cardHeaderWrapper}>
+            <h2 className={styles.cardHeader}>{props.name}</h2>
+          </div>
+          <div className={styles.cardImageWrapper}>
+            <Image
+              className={styles.cardImage}
+              alt='coffee shop'
+              src={props.imgUrl}
+              width={260}
+              height={160}
+            />
+          </div>
+        </div>
       </a>
     </Link>
   );
